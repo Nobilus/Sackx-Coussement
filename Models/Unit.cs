110513e6c93357e3f38df99908c70c053f84f503
JonasDM <jonasdemeyer@outlook.com>
2021-04-15 14:36:16 +0200

    configure docker

---
+++ Models/Unit.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Project.Models
+{
+    public class Unit
+    {
+        public Guid UnitId { get; set; }
+        public string Naam { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Project.Config;
 
 namespace Project
 {
@@ -32,6 +33,9 @@ namespace Project
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Project", Version = "v1" });
             });
+
+            // connectionstrings:
+            services.Configure<ConnectionStrings>(Configuration.GetSection("ConnectionStrings"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
