dd7847ff2edb02d883c5b1310116de9966643c90
JonasDM <jonasdemeyer@outlook.com>
2021-04-30 10:32:48 +0200

    build project

--- Woodshop.Test/WoodshopControllerTest.cs
+++ Woodshop.Test/WoodshopControllerTest.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace Woodshop.Test
 {
-    public class UnitTest1
+    public class WoodshopControllerTest: IClassFixture<WebApplicationFactory<Woodshop.API.Startup>>
     {
         [Fact]
         public void Test1()
