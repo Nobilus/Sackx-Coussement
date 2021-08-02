2526f111b0ff75328e20705c069b1b79e346efed
JonasDM <jonasdemeyer@outlook.com>
2021-08-02 11:54:52 +0200

    basic form html

--- Frontend/pages/login.tsx
+++ Frontend/pages/login.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useFormik } from "formik";
 
 function Login() {
-  return <div></div>;
+  const formik = useFormik({
+    initialValues: { username: "", password: "" },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="username">Gebruikersnaam</label>
+      <input
+        type="text"
+        name="username"
+        id="username"
+        onChange={formik.handleChange}
+        value={formik.values.username}
+      />
+      <label htmlFor="password">Wachtwoord</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default Login;
