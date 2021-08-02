e1857eb99f3293ae9d2d1ec83ddcf27129645364
JonasDM <jonasdemeyer@outlook.com>
2021-08-02 12:03:55 +0200

    add default backgorund styling

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,7 +1,14 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      primary: "#FAFCFA",
+    }),
+    fontFamily: {},
     extend: {},
   },
   variants: {
