2f10d21dd98da1c0efc2a2a27bf6cc3699c84471
JonasDM <jonasdemeyer@outlook.com>
2021-08-31 10:08:20 +0200

    add footoer

--- Frontend/components/Footer/index.tsx
+++ Frontend/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react";
 import Customer from "../../public/assets/Customer.svg";
-import Trunk from "../../public/assets/Trunk.svg";
-import Image from "next/image";
+import Trunks from "../../public/assets/Trunks.svg";
+import Bestelbonnen from "../../public/assets/Bestelbonnen.svg";
 
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -9,18 +9,37 @@ import { useRouter } from "next/router";
 const Footer: FunctionComponent = () => {
   const { pathname } = useRouter();
 
+  function isActive(routename: string) {
+    if (pathname !== routename) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   return (
-    <footer className="h-10 bg-white flex flex-row justify-center items-center">
-      <a className="px-2">
-        <Link href="/producten">producten</Link>
-      </a>
-      <a className="px-2">
-        <Link href="/bestelbonnen">bestelbonnen</Link>
-      </a>
+    <footer className="bg-white flex flex-row justify-center items-center">
+      <Link href="/">
+        <a className="flex flex-col content-center m-auto">
+          {/* <Trunks fill={isActive("producten") ? "#00371C" : "#668776"} /> */}
+
+          <p className="m-auto">Producten</p>
+        </a>
+      </Link>
+
+      <Link href="/bestelbonnen">
+        <a className="flex flex-col content-center m-auto">
+          {/* <Bestelbonnen
+            fill={isActive("bestelbonnen") ? "#00371C" : "#668776"}
+          /> */}
+          <p className="m-auto">Bestelbonnen</p>
+        </a>
+      </Link>
+
       <Link href="/klanten">
-        <a>
-          <Image src={Customer} alt="klanten" />
-          <p>Klanten</p>
+        <a className="flex flex-col content-center m-auto">
+          {/* <Customer fill={isActive("klanten") ? "#00371C" : "#668776"} /> */}
+          <p className="m-auto">Klanten</p>
         </a>
       </Link>
     </footer>
