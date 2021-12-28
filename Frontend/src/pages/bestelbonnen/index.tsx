1f3223253e192888bf2491de3e05b063fbc8f700
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-12-28 15:16:07 +0100

    move table components

--- Frontend/src/pages/bestelbonnen/index.tsx
+++ Frontend/src/pages/bestelbonnen/index.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import PageLayout from "src/components/Layout/PageLayout";
+import { Bestelbon } from "src/types/Bestelbon";
+
+const bestelbonnen: Array<Bestelbon> = [
+  {
+    customer: "Tom De Meyer",
+    entries: [
+      { date: "01/02/03", amount: 320.5 },
+      { date: "01/02/03", amount: 320.5 },
+      { date: "01/02/03", amount: 320.5 },
+      { date: "01/02/03", amount: 320.5 },
+      { date: "01/02/03", amount: 320.5 },
+      { date: "01/02/03", amount: 320.5 },
+    ],
+  },
+  {
+    customer: "Jonas De Meyer",
+    entries: [
+      { date: "01/02/03", amount: 320.5 },
+      { date: "01/02/03", amount: 320.5 },
+      { date: "01/02/03", amount: 320.5 },
+      { date: "01/02/03", amount: 320.5 },
+      { date: "01/02/03", amount: 320.5 },
+      { date: "01/02/03", amount: 320.5 },
+    ],
+  },
+];
 
 const Bestelbonnen = () => {
-  return <div></div>;
+  return <PageLayout></PageLayout>;
 };
 
 export default Bestelbonnen;
---
+++ Frontend/src/types/Bestelbon.ts
@@ -0,0 +1,9 @@
+interface BestelbonEntry {
+  date: string;
+  amount: number;
+}
+
+export interface Bestelbon {
+  customer: string;
+  entries: Array<BestelbonEntry>;
+}
