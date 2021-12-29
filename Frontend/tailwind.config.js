a37adbf4458eadde2613136f28cf4c5765258115
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-12-29 17:31:34 +0100

    update tailwind.config.js to v3

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,20 +2,18 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
-  purge: {
-    content: [
-      "src/pages/**/*.{js,ts,jsx,tsx}",
-      "src/components/**/*.{js,ts,jsx,tsx}",
-    ],
-    safelist: [
-      "grid-cols-1",
-      "grid-cols-2",
-      "grid-cols-3",
-      "grid-cols-4",
-      "grid-cols-5",
-      "grid-cols-6",
-    ],
-  },
+  content: [
+    "src/pages/**/*.{js,ts,jsx,tsx}",
+    "src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    "grid-cols-1",
+    "grid-cols-2",
+    "grid-cols-3",
+    "grid-cols-4",
+    "grid-cols-5",
+    "grid-cols-6",
+  ],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
