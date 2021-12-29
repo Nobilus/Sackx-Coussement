81827a5b528ac2d101c5e724aa10b86d3a92798f
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-12-29 15:51:52 +0100

    install nextpwa and favicons

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,6 +1,12 @@
-const moduleExports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
   distDir: "dist",
-};
-
-module.exports = moduleExports;
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
--- Frontend/src/pages/_document.tsx
+++ Frontend/src/pages/_document.tsx
@@ -15,22 +15,26 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="/favicons/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="/favicons/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="/favicons/favicon-16x16.png"
           />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#003415" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicons/safari-pinned-tab.svg"
+            color="#003415"
+          />
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
